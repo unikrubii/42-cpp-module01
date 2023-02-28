f203e192d2c1a5798fe1c459cd6d9ff5027fb22f
Sirilert Thitikul <rubii@Rubii-Mac.local>
2023-02-28 23:09:31 +0700

    fix usage

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -2,7 +2,7 @@
 
 int main( int argc, char **argv ) {
 	if (argc != 4) {
-		std::cerr << RED << "Usage: ./replace <filename> <s1> <s2>" << RES << std::endl;
+		std::cerr << RED << "Usage: ./sed <filename> <s1> <s2>" << RES << std::endl;
 		return 1;
 	}
 	Sed sed( argv[ 1 ], argv[ 2 ], argv[ 3 ] );
